f322f1a9f06333e4d6612ac7ddc8aadf64d0dd1d
KazukiDaido <k10de302222@gmail.com>
2023-02-10 04:36:01 +0900

    Initial Commit

---
+++ TextLabel/TextLabelApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextLabelApp.swift
+//  TextLabel
+//
+//  Created by 大道一輝 on 2023/02/10.
+//
+
+import SwiftUI
+
+@main
+struct TextLabelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
