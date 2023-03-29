47e9d7cae1787b8ccf3dfb25d1760bc58e9bd048
KazukiDaido <k10de302222@gmail.com>
2023-03-30 08:53:28 +0900

    リストスワイプで削除ボタン表示（ボタン表示のみ）

--- TextLabel/ContentView.swift
+++ TextLabel/ContentView.swift
@@ -16,45 +16,61 @@ struct ContentView: View {
     
     var body: some View {
         
-        VStack {
-            Text(textName)
-            Text(textName)
-            Text(textName)
-            HStack {
-                Text(textName)
-                Text(textName)
-                Text(textName)
-            }
-            TextField("名前を入力", text: $name)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .multilineTextAlignment(.center)
-            Button("名前変更ボタン") {
-                if name == "" {
-                    textName = "Hello world!"
-                } else {
-                    textName = name
-                }
-            }
-            
-            NavigationView{
-                List {
-                    ForEach(0 ..< modelData.fruits.count, id: \.self) { index in
-                        NavigationLink {
-                            FruitDetail(fruits: modelData.fruits[index])
-                        } label: {
-                            Text(modelData.fruits[index].name)
-                        }
+        /*
+         VStack {
+         Text(textName)
+         Text(textName)
+         Text(textName)
+         HStack {
+         Text(textName)
+         Text(textName)
+         Text(textName)
+         }
+         TextField("名前を入力", text: $name)
+         .textFieldStyle(RoundedBorderTextFieldStyle())
+         .multilineTextAlignment(.center)
+         Button("名前変更ボタン") {
+         if name == "" {
+         textName = "Hello world!"
+         } else {
+         textName = name
+         }
+         }
+         
+         */
+        NavigationView{
+            List {
+                ForEach(0 ..< modelData.fruits.count, id: \.self) { index in
+                    NavigationLink {
+                        FruitDetail(fruits: modelData.fruits[index])
+                    } label: {
+                        Text(modelData.fruits[index].name)
                     }
                 }
+                .swipeActions(edge: .trailing, allowsFullSwipe: true) {
+                    Button {
+                        print()
+                    } label: {
+                        Text("削除")
+                    }
+                    .tint(.red)
+                }
+                // .onDelete(perform: rowRemove)
             }
         }
+        /*func rowRemove(offsets: IndexSet) {
+         modelData.fruits.remove(atOffsets: offsets)
+         }
+         */
     }
 }
-
-struct ContentView_Previews: PreviewProvider {
-    static let modelData = ModelData()
-    static var previews: some View {
-        ContentView()
-            .environmentObject(modelData)
+    
+    
+    
+    struct ContentView_Previews: PreviewProvider {
+        static let modelData = ModelData()
+        static var previews: some View {
+            ContentView()
+                .environmentObject(modelData)
+        }
     }
-}
--- TextLabel/Model/ModelData.swift
+++ TextLabel/Model/ModelData.swift
@@ -16,8 +16,11 @@ final class ModelData: ObservableObject {
     
     
     func load() {
-        let url = URL(string: "https://us-central1-fruitsapi.cloudfunctions.net/fruits")!  //URLを生成
+        let url = URL(string:"https://us-central1-fruitsapi.cloudfunctions.net/fruits")!  //URLを生成
         let request = URLRequest(url: url)//Requestを生成
+        //request.httpMethod = "GET"
+        //request.httpBody = "name=pineapple&sale=true&value=6000".data(using: .utf8)
+        
         let task = URLSession.shared.dataTask(with: request) { (data, response, error) in  //非同期で通信を行う
             guard let data = data else { return }
             do {
@@ -27,7 +30,7 @@ final class ModelData: ObservableObject {
                 DispatchQueue.main.async {
                     self.fruits = response.data.reversed()
                 }
-                
+                print("--------------------posted")
             } catch {
                 fatalError("Couldn't parse as \(Fruit.self):\n\(error)")
             }
